c1fbd297549b3d14426a5f41ea98c1d944611d08
Kishore Singireddy <official4kishore@gmail.com>
2025-05-23 23:59:19 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-AMO_ID="ami-09c813fb71547fc4f"
+AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-06e1b399be05bca65"
 ZONE_ID="Z02669663M5NZUABP37AR"
 ZONE_NAME="singireddy.shop"
-INSTANCE_NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-for instance in ${INSTANCE_NAME[@]}
+for instance in ${INSTANCES[@]}
 do
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instance[0].InstanceID" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-06e1b399be05bca65 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instance[0].InstanceID" --output text)
 
     if [ $instance != "frontend" ]
     then
