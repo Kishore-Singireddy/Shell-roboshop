323cc4274634bfc155ff918a4b5d6f6b3638a7df
Kishore Singireddy <official4kishore@gmail.com>
2025-05-24 12:22:41 +0530

    roboshop shell

---
+++ 01pract.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-06e1b399be05bca65"
+ZONE_ID="Z02669663M5NZUABP37AR"
+ZONE="singireddy.shop"
+INSTANCES=("mongodb" "redis" "mysql" "rabitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
+for instances in {$INSTANCES[@]}
+do
+ echo " This is instances - $instances "
+ echo " This is INSTANCES - $INSTANCES "
+done
+
