37b896c80e6613d1dede05fd769db3c093dd5c15
Kishore Singireddy <official4kishore@gmail.com>
2025-05-24 16:58:17 +0530

    roboshop shell

---
+++ 02delete_instance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This script is for deleting AWS instances
+
+#aws ec2 describe-instances
+
+#aws ec2 terminate-instances --instance-ids i-5203422c
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-06e1b399be05bca65"
+ZONE_ID="Z02669663M5NZUABP37AR"
+ZONE="singireddy.shop"
+INSTANCES=("mongodb" "redis" "mysql" "rabitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
+#Query to list all the running ec2 instances - gives the instance ids
+INSTANCES_ID=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=terminated --output text)
+
+echo "$INSTANCES_ID"
