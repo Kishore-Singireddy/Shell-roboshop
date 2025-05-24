321291a7a7d17f97190e37056d2a007de79567a7
Kishore Singireddy <official4kishore@gmail.com>
2025-05-24 13:00:08 +0530

    roboshop shell

--- 01pract.sh
+++ 01pract.sh
@@ -6,8 +6,16 @@ ZONE="singireddy.shop"
 INSTANCES=("mongodb" "redis" "mysql" "rabitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
 for instances in ${INSTANCES[@]}
+
 do
-    echo " This is instances - $instances "
-    echo " This is INSTANCES - ${INSTANCES[@]} "
+$INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGI_ID  "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+echo "$INSTANCE_ID"
+
+    # if [ $instance != "frontend" ]
+    # then
+    #     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+
+    # echo " This is instances - $instances "
+    # echo " This is INSTANCES - ${INSTANCES[@]} "
 done
 
