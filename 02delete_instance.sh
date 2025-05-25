eb5553360821f96e6e6b28512e79cdb6d0921b11
Kishore Singireddy <official4kishore@gmail.com>
2025-05-25 12:47:14 +0530

    roboshop shell

--- 02delete_instance.sh
+++ 02delete_instance.sh
@@ -24,7 +24,7 @@ if [ $INSTANCES_COUNT != 0 ]
 then
     for instances_id in ${INSTANCES_ID[@]}
     do
-        #aws ec2 terminate-instances --instance-id $instances_id
+        aws ec2 terminate-instances --instance-id $instances_id
         REM_INSTANCES_COUNT=$($INSTANCES_COUNT -1)
         echo " These are remaining instances $REM_INSTANCES_COUNT "
         # if [ $REM_INSTANCES_COUNT == 0 ]
